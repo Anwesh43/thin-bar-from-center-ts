e20221ab92e154b895a81ba904495430954697ad
Anwesh43 <anweshthecool0@gmail.com>
2021-05-23 20:26:16 +0530

    useStyle hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react'
+import {CSSProperties, useEffect, useState} from 'react'
 
 const scGap : number = 0.02 
 const delay : number = 20 
@@ -39,4 +39,59 @@ export const useDimension = () => {
     return {
         w, h
     }
+}
+
+export const useStyle = (w : number, h : number, scale : number) {
+    const sf : number = Math.sin(scale * Math.PI)
+    const bw : number = Math.min(w, h) / 9
+    const sw : number = Math.min(w, h) / 15 
+    const position = 'absolute'
+    const x : number = w / 2
+    const y : number = h / 2
+    const background : string = "indigo"
+    return {
+        blockStyle() : CSSProperties {
+            const width = `${bw}px`
+            const height = `${bw}px`
+            const left = `${w / 2 - bw / 2}px`
+            const top = `${h / 2 - bw / 2}px`
+            return {
+                position, 
+                width, 
+                height, 
+                top, 
+                left,
+                background 
+             }
+        },
+        barXStyle(i : number) : CSSProperties {
+            const left = `${(w / 2 - bw / 2) * (1 - sf) * (1 - i) + (w / 2 + bw / 2) * i}px`
+            const top = `${h / 2 - bw / 2}px`
+            const width = `${(w / 2 - bw / 2) * scale}px`
+            const height = `${bw}px`
+            return {
+                left, 
+                top, 
+                width, 
+                height, 
+                position, 
+                background 
+            }
+
+        },
+        barYStyle(i : number) : CSSProperties {
+            const left = `${bw}px`
+            const top = `${(h / 2 - bw / 2) * (1 - sf) * (1 - i) + (h / 2 + bw / 2) * i}px`
+            const width = `${bw}px`
+            const height = `${(h / 2 - bw / 2) * sf}px`
+            return {
+                position, 
+                left,
+                top, 
+                width, 
+                height,
+                background 
+            }
+        }
+    } 
 }
